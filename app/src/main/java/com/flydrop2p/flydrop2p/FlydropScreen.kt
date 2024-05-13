0c936ac622f6894908f8c04dcd5264c0fb3ded8a
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-05-13 13:03:56 +0200

    topAppBar

--- app/src/main/java/com/flydrop2p/flydrop2p/FlydropScreen.kt
+++ app/src/main/java/com/flydrop2p/flydrop2p/FlydropScreen.kt
@@ -1,21 +1,31 @@
 package com.flydrop2p.flydrop2p
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -59,19 +69,57 @@ fun FlydropTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ) {
-    CenterAlignedTopAppBar(title = { Text(title) },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior,
+
+    TopAppBar(
         navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
+            if (canNavigateBack){
+                IconButton(onClick = {
+                    navigateUp()
+                }) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
+                        contentDescription = "Go back"
                     )
                 }
             }
-        })
+        },
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(
+                    onClick = { /* Action for custom icon */ }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.wifi_tethering_24px),
+                        contentDescription = "Settings",
+                        modifier = Modifier.size(36.dp)
+                    )
+                }
+                IconButton(
+                    onClick = { /* Action for settings icon */ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Settings",
+                        modifier = Modifier.size(36.dp)
+                    )
+                }
+            }
+
+        },
+        colors =  TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+        ),
+        modifier = modifier.padding(top = 16.dp, bottom = 8.dp)
+    )
 }
 
 @Preview
