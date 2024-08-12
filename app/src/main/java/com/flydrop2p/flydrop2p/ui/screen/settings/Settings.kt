142b9c2e7d0b133cbc510092f869b58cbc9df151
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-08-12 21:45:37 +0200

    added settings screen

--- app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/settings/Settings.kt
+++ app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/settings/Settings.kt
@@ -1,9 +1,31 @@
 package com.flydrop2p.flydrop2p.ui.screen.settings
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.flydrop2p.flydrop2p.FlyDropTopAppBar
 import com.flydrop2p.flydrop2p.R
@@ -38,5 +60,45 @@ fun SettingsScreen(
         },
     ) {
 
+        Log.d("SettingsScreen", "SettingsScreen")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.account_circle_24px),
+                    colorFilter = ColorFilter.tint(Color.Black),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column {
+                    Text(
+                        text = "Name: ${settingsViewModel.userName.value}",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Device: ${settingsViewModel.deviceName.value}",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/settings/SettingsViewModel.kt
+++ app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/settings/SettingsViewModel.kt
@@ -1,7 +1,15 @@
 package com.flydrop2p.flydrop2p.ui.screen.settings
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class SettingsViewModel : ViewModel() {
+    private val _userName = mutableStateOf("Giovanni Rossi")
+    val userName = _userName
 
+    private val _deviceName = mutableStateOf("Iphone di Giovanni")
+    val deviceName = _deviceName
+
+    private val _profilePicture = mutableStateOf("URL o risorsa per l'immagine di profilo")
+    val profilePicture = _profilePicture
 }
