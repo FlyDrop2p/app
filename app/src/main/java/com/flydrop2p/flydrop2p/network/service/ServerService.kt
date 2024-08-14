a68b243c69a138edbd0d2490d23b9bebf0c8917e
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-08-15 00:04:17 +0200

    changed the theme colors and setup file sharing

--- app/src/main/java/com/flydrop2p/flydrop2p/network/service/ServerService.kt
+++ app/src/main/java/com/flydrop2p/flydrop2p/network/service/ServerService.kt
@@ -6,12 +6,15 @@ import com.flydrop2p.flydrop2p.network.model.Keepalive
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.FileOutputStream
 import java.net.ServerSocket
 
 class ServerService {
     companion object {
         const val PORT_KEEPALIVE: Int = 8800
         const val PORT_CONTENT_STRING: Int = 8801
+        const val PORT_FILE_TRANSFER: Int = 8802
     }
 
     suspend fun listenKeepalive(): Keepalive {
@@ -50,5 +53,32 @@ class ServerService {
         }
 
         return ret
+    }
+
+    suspend fun receiveFile(destination: File): Boolean {
+        return withContext(Dispatchers.IO) {
+            try {
+                val serverSocket = ServerSocket(PORT_FILE_TRANSFER)
+                val client = serverSocket.accept()
+
+                val inputStream = client.getInputStream()
+                val fileOutputStream = FileOutputStream(destination)
+
+                val buffer = ByteArray(1024)
+                var bytesRead: Int
+
+                while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead)
+                }
+
+                fileOutputStream.close()
+                inputStream.close()
+                serverSocket.close()
+                true
+            } catch (e: Exception) {
+                Log.e("FILE TRANSFER", "Errore durante il trasferimento del file", e)
+                false
+            }
+        }
     }
 }
