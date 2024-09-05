fc8ee115bcd83b867881d6354ca7c6682a62cebb
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-09-05 08:05:56 +0200

    fixed top bar fixed

--- app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/chat/ChatScreen.kt
+++ app/src/main/java/com/flydrop2p/flydrop2p/ui/screen/chat/ChatScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -99,6 +100,7 @@ fun ChatScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .imePadding()
         ) {
             currentAccount?.let { account ->
                 MessagesList(
